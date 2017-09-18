a68091b7057ca0d014e4ce20fd903c1f7346f736
Ber Cohen <thegreatber@gmail.com>
2017-09-18 15:10:56 -0400

    first commit beravel 3

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+       <h1>{{$title}}</h1>
+        <p>
+            Hey!
+            My name is Ber and I am a young web developer trying to constantly expand my knowledge and skills!
+            I have experience building small applications in Ruby on Rails and a working understanding of C# .NET.
+            Both are MVC aka "Models Views Controllers" frameworks.
+            In attempt to expand my horizons, I took a crack at the PHP framework "Laravel".
+            I was delighted to find Laravel to be smooth and sleek, with almost no need for long wordy code lines.
+            The syntax is sharp and direct making it a clean and fun coding experience!
+            I made this basic blog with the help of some great youtube tutorials.
+            It is complete with Authentication, CRUD, aka "Create Read Update Delete", and file upoading functionality.
+            Feel free to give it a try! 
+            You can use Email: "john@gmail.com" Password: "123456" to login.
+            Cheers!
+        </p>
+@endsection
